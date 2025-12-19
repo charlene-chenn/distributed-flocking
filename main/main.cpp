d8467a6043843488e4f3da982ba7ff3abc3ac274
Charlene Chen <57976532+charlene-chenn@users.noreply.github.com>
2025-12-19 15:35:00 +0000

    Update main

--- main/main.cpp
+++ main/main.cpp
@@ -555,7 +555,7 @@ extern "C" void radio_task(void *pvParameters) {
         int64_t current_time_ms = task_start_us / 1000;
 
         // Transmit phase
-        if (current_time_ms - last_tx_time_ms >= pdTicks_TO_MS(xRadioPeriodTicks)) {
+        if (current_time_ms - last_tx_time_ms >= 6000) {
             last_tx_time_ms = current_time_ms;
             lora->standby();
 
